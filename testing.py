eb61c773db5e2bd375911d9d930b433f73ced55b
dornab <dorna.bandari@gmail.com>
2016-04-18 21:57:41 -0700

    Testing testing!

---
+++ testing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import datetime
+import pprint
+#fr=open("/Users/Dorna/Documents/ServerDir/data/stripe/data.csv", "r")
+#fw=open("/Users/Dorna/Documents/ServerDir/data/stripe/data_mod.csv", "w")
+
+
+def fizbuzz2(N):
+    for i in range(1,N+1):
+        outString = ""
+        if i % 3 == 0:
+            outString= "Fiz"
+        if i%5==0:
+            outString+= "Buzz"
+        
+        print outString or str(i)
+
+fizbuzz2(16)
+
+#pprint.pprint(data)
+#fr.close()
+#fw.close()
